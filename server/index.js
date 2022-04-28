098f9fdf0cd1fb457159d99ac710a972c1c25f60
Jose Molina <josemolinaf22@gmail.com>
2022-04-28 14:38:24 -0600

    finished

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const path = require('path')
 
 const app = express()
 
+app.use(express.json())
+
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
 var rollbar = new Rollbar({
@@ -17,6 +19,33 @@ rollbar.log('Hello world!')
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, '../index.html'))
 })
+
+app.get('/styles', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../styles.css'))
+    rollbar.info('css file served')
+})
+
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    const index = students.findIndex(studentName=> studentName === name)
+
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
+
+})
+
 const port = process.env.PORT || 4545
+app.use(rollbar.errorHandler())
 
 app.listen(port, ()=>  console.log(`Take us to warp ${port}!`))
