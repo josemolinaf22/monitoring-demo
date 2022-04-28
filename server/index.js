e1fab6ba781e14e327e5e9a77815aeaf52c7be33
Jose Molina <josemolinaf22@gmail.com>
2022-04-28 14:47:57 -0600

    finished part 2

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-app.get('/styles', (req, res)=>{
+app.get('/style', (req, res)=>{
     res.sendFile(path.join(__dirname, '../styles.css'))
     rollbar.info('css file served')
 })
